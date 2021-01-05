89fb27c59e33268e703a94ad09274fff5c46a385
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2021-01-05 11:46:24 -0800

    add check on series during book creation

--- src/handlers/book/postBook.js
+++ src/handlers/book/postBook.js
@@ -1,6 +1,7 @@
 import createError from 'http-errors';
 import dbCreateBook from '../../lib/dbCreateBook';
 import commonMiddleware from '../../lib/commonMiddleware';
+import dbQuerySeriesById from '../../lib/dbQuerySeriesById';
 
 async function postBook(event) {
   const { name, seriesId } = event.body;
@@ -13,6 +14,13 @@ async function postBook(event) {
   }
 
   try {
+    const series = await dbQuerySeriesById(seriesId);
+    if (!series) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: `Series with ID "${seriesId}" not found.` }),
+      };
+    }
 
     const newBook = await dbCreateBook(name, seriesId);
 
