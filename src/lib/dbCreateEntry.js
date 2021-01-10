61c0ea8f8970a714202e37e7c3752b3cb19be588
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2021-01-09 21:37:40 -0800

    add create entry endpoint

--- src/lib/dbCreateEntry.js
+++ src/lib/dbCreateEntry.js
@@ -3,23 +3,24 @@ import generateId from './base64id';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-export default async function dbCreateBook(name, seriesId) {
+export default async function dbCreateEntry(text, bookId, seriesId) {
   const now = new Date();
 
-  const book = {
-    primary_key: seriesId,
-    sort_key: `BOOK~${generateId(10)}`,
-    name,
+  const entry = {
+    primary_key: bookId,
+    sort_key: `ENTRY~${generateId(10)}`,
+    text,
+    seriesId,
     createdAt: now.toISOString(),
     updatedAt: now.toISOString()
   }
 
   const params = {
     TableName: process.env.NO_SPOILERS_TABLE_NAME,
-    Item: book
+    Item: entry
   };
 
   await dynamodb.put(params).promise();
 
-  return book;
+  return entry;
 }
