207dbc2475765430f5093ff67efda4ee99bd8a8f
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2021-05-11 16:46:52 -0700

    increase ID complexity

--- src/lib/base64id.js
+++ src/lib/base64id.js
@@ -1,7 +1,9 @@
-const base = [...'abcdefghijklmnopqrstuvwxyz0123456789'];
+const base = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'];
+const numbers = [...'0123456789'];
 
 export default function generateId (size) {
-   return [...Array(size)]
+   return numbers[Math.random()*numbers.length|0] +
+     [...Array(size)]
      .map(() => base[Math.random()*base.length|0])
      .join('');
 }
