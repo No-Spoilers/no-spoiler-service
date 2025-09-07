0d480f39384e8e0101fd71cd8a4d9f6897a65c7c
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2025-09-07 12:46:19 -0700

    add some generic util functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,49 @@
+import type { AttributeValue } from '@aws-sdk/client-dynamodb';
+import createError from 'http-errors';
+
+export function validateString(value: unknown): string {
+  if (typeof value !== 'string') {
+    throw badRequestError('expected a string, but got ' + typeof value);
+  }
+  return value;
+}
+
+export function extractStringValue(
+  attrValue: AttributeValue | undefined,
+): string {
+  if (typeof attrValue?.S === 'string') {
+    return attrValue.S;
+  }
+  return '';
+}
+
+export function success(body: unknown) {
+  return {
+    statusCode: 200,
+    body,
+  };
+}
+
+// 400
+export function badRequestError(message: string) {
+  console.error('badRequestError:', message);
+  return new createError.BadRequest(message);
+}
+
+// 401
+export function unauthorizedError(message: string) {
+  console.error('unauthorizedError:', message);
+  return new createError.Unauthorized(message);
+}
+
+// 404
+export function notFoundError(contentId: string) {
+  console.error('notFoundError:', contentId);
+  return new createError.NotFound(`contentId:${contentId} not found`);
+}
+
+// 500
+export function internalServerError(error: unknown) {
+  console.error('internalServerError:', error);
+  return new createError.InternalServerError(String(error));
+}
